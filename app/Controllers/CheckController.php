7e9be3eb9c486f829e50dc28810fd2f756166086
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-09-10 16:34:07 +0200

    adds messaging

--- app/Controllers/CheckController.php
+++ app/Controllers/CheckController.php
@@ -4,9 +4,31 @@
 namespace App\Controllers;
 
 use Kernel\Controller;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class CheckController extends Controller
 {
+    private $connection;
+    private $channel;
+
+    public function __construct($data = null, $database = null)
+    {
+        parent::__construct($data, $database);
+
+        $this->connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $this->channel = $this->connection->channel();
+
+        $this->channel->queue_declare('hello', false, false, false, false);
+
+    }
+
+    public function __destruct()
+    {
+        $this->channel->close();
+        $this->connection->close();
+    }
+
     protected function get_index()
     {
         return view('home.check');
@@ -18,14 +40,15 @@ class CheckController extends Controller
         $urls = explode("\n", trim($urls));
         $urls = array_map('trim', $urls);
 
-        $result = [];
-        foreach ($urls as $url) {
-            $result[$url] = $this->get_http_code($url);
+////// Messaging to Consumer
+        foreach($urls as $url){
+            $msg = new AMQPMessage($url);
+            $this->channel->basic_publish($msg, '', 'hello');
         }
 
         return view('home.check', [
-            'akbar' => $result,
-            'input' => array_keys($result)
+//            'akbar' => $result,
+//            'input' => array_keys($result)
         ]);
     }
 
